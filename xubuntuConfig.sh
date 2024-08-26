f9fab81d06888760c4015655f076a31fd8cda013
leodionisiolima <69328311+leodionisiolima@users.noreply.github.com>
2024-08-26 13:17:24 -0300

    Update xubuntuConfig.sh

--- xubuntuConfig.sh
+++ xubuntuConfig.sh
@@ -17,11 +17,11 @@ echo -e "${boldGreen}\nCriando diretórios para o usuário $currentUser...\n${re
 sudo -u "$currentUser" mkdir -p "/home/$currentUser/Desktop" "/home/$currentUser/Documents" "/home/$currentUser/Downloads" "/home/$currentUser/Pictures" "/home/$currentUser/Videos" "/home/$currentUser/Music" "/home/$currentUser/Public" "/home/$currentUser/Templates"
 sudo -u "$currentUser" mkdir -p "/home/$currentUser/.config/xfce4"
 
-# Atualizar o sistema e instalar pacotes desejados com Nala
-echo -e "${boldGreen}\nAtualizando o sistema e instalando pacotes com Nala...\n${reset}"
-sudo nala update && sudo nala upgrade -y && \
+# Atualizar o sistema e instalar pacotes desejados
+echo -e "${boldGreen}\nAtualizando o sistema e instalando pacotes...\n${reset}"
+sudo apt update && sudo apt upgrade -y && \
 echo -e "${boldGreen}\nInstalando pacotes que eu sempre uso...\n${reset}"
-sudo nala install -y most wmctrl sc-im xdotool nodejs npm p7zip-full bat vim git curl wget build-essential software-properties-common alacritty acpi acpid bc blueman brightnessctl btop cron dpkg dpkg-dev efibootmgr elementary-xfce-icon-theme gimp gimp-data gimp-help-common gimp-help-en gnome-keyring htop iotop locales login lua-luv make man-db manpages manpages-dev mate-calc mate-calc-common micro mount mousepad mugshot nala nano neovim neovim-runtime pandoc pandoc-data parted passwd pipewire pipewire-bin qt5-gtk-platformtheme rclone ripgrep rsync rsyslog sudo systemd tar terminator thunar thunar-archive-plugin thunar-data thunar-media-tags-plugin thunar-volman time timeshift tmux tree ubuntu-keyring vim virtualbox virtualbox-dkms virtualbox-qt wget wireless-tools xcursor-themes xcvt xdg-dbus-proxy xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils xfburn xfce4-appfinder xfce4-clipman xfce4-clipman-plugin xfce4-cpugraph-plugin xfce4-dict xfce4-helpers xfce4-indicator-plugin xfce4-mailwatch-plugin xfce4-netload-plugin xfce4-notes xfce4-notes-plugin xfce4-notifyd xfce4-panel xfce4-panel-profiles xfce4-places-plugin xfce4-power-manager xfce4-power-manager-data xfce4-power-manager-plugins xfce4-pulseaudio-plugin xfce4-screensaver xfce4-screenshooter xfce4-session xfce4-settings xfce4-systemload-plugin xfce4-taskmanager xfce4-terminal xfce4-verve-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin xfconf xfdesktop4 xfdesktop4-data xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xfwm4 x11-xserver-utils python3 python3-apt python3-apport python3-attr python3-babel python3-bs4 python3-cairo python3-certifi python3-cffi python3-cffi-backend python3-chardet python3-charset-normalizer python3-click python3-colorama python3-commandnotfound python3-cryptography python3-dateutil python3-dbus python3-debconf python3-debian python3-distro python3-distupgrade python3-flake8 python3-full python3-gdbm python3-gi python3-gi-cairo python3-html5lib python3-httplib2 python3-idna python3-importlib-metadata python3-jinja2 python3-keyring python3-lxml python3-mako python3-markdown python3-markupsafe python3-minimal python3-msgpack python3-neovim python3-netifaces python3-oauthlib python3-packaging python3-pexpect python3-pil python3-pip python3-pkg-resources python3-psutil python3-ptyprocess python3-py python3-pycodestyle python3-pycparser python3-pyflakes python3-pygments python3-pyinotify python3-pynvim python3-pyparsing python3-pytest python3-pytest-cov python3-reportlab python3-requests python3-secretstorage python3-setuptools python3-six python3-soupsieve python3-toml python3-typing-extensions python3-update-manager python3-urllib3 python3-yaml || { echo -e "${boldRed}\nFalha ao instalar pacotes.\n${reset}"; exit 1; }
+sudo apt install -y most wmctrl sc-im xdotool nodejs npm p7zip-full bat vim git curl wget build-essential software-properties-common alacritty acpi acpid bc blueman brightnessctl btop cron dpkg dpkg-dev efibootmgr elementary-xfce-icon-theme gimp gimp-data gimp-help-common gimp-help-en gnome-keyring htop iotop locales login lua-luv make man-db manpages manpages-dev mate-calc mate-calc-common micro mount mousepad mugshot nano neovim neovim-runtime pandoc pandoc-data parted passwd pipewire pipewire-bin qt5-gtk-platformtheme rclone ripgrep rsync rsyslog sudo systemd tar terminator thunar thunar-archive-plugin thunar-data thunar-media-tags-plugin thunar-volman time timeshift tmux tree ubuntu-keyring vim virtualbox virtualbox-dkms virtualbox-qt wget wireless-tools xcursor-themes xcvt xdg-dbus-proxy xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils xfburn xfce4-appfinder xfce4-clipman xfce4-clipman-plugin xfce4-cpugraph-plugin xfce4-dict xfce4-helpers xfce4-indicator-plugin xfce4-mailwatch-plugin xfce4-netload-plugin xfce4-notes xfce4-notes-plugin xfce4-notifyd xfce4-panel xfce4-panel-profiles xfce4-places-plugin xfce4-power-manager xfce4-power-manager-data xfce4-power-manager-plugins xfce4-pulseaudio-plugin xfce4-screensaver xfce4-screenshooter xfce4-session xfce4-settings xfce4-systemload-plugin xfce4-taskmanager xfce4-terminal xfce4-verve-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin xfconf xfdesktop4 xfdesktop4-data xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xfwm4 x11-xserver-utils python3 python3-apt python3-apport python3-attr python3-babel python3-bs4 python3-cairo python3-certifi python3-cffi python3-cffi-backend python3-chardet python3-charset-normalizer python3-click python3-colorama python3-commandnotfound python3-cryptography python3-dateutil python3-dbus python3-debconf python3-debian python3-distro python3-distupgrade python3-flake8 python3-full python3-gdbm python3-gi python3-gi-cairo python3-html5lib python3-httplib2 python3-idna python3-importlib-metadata python3-jinja2 python3-keyring python3-lxml python3-mako python3-markdown python3-markupsafe python3-minimal python3-msgpack python3-neovim python3-netifaces python3-oauthlib python3-packaging python3-pexpect python3-pil python3-pip python3-pkg-resources python3-psutil python3-ptyprocess python3-py python3-pycodestyle python3-pycparser python3-pyflakes python3-pygments python3-pyinotify python3-pynvim python3-pyparsing python3-pytest python3-pytest-cov python3-reportlab python3-requests python3-secretstorage python3-setuptools python3-six python3-soupsieve python3-toml python3-typing-extensions python3-update-manager python3-urllib3 python3-yaml || { echo -e "${boldRed}\nFalha ao instalar pacotes.\n${reset}"; exit 1; }
 
 
 # Clonar repositórios de temas e instalar
@@ -94,13 +94,18 @@ else
     exit 1
 fi
 
-# Avisando que vai criar um link simbólico para nala como apt
-echo -e "${boldGreen}Criando link simbólico de apt para nala...${reset}"
-sudo ln -sf /usr/bin/nala /usr/bin/apt
-if [ $? -eq 0 ]; then
-    echo -e "${boldGreen}Link simbólico de apt para nala criado com sucesso.${reset}"
+echo -e "${boldGreen}Verificando se o Nala está instalado...${reset}"
+if command -v nala >/dev/null 2>&1; then
+    echo -e "${boldGreen}Criando link simbólico de apt para nala...${reset}"
+    sudo ln -sf /usr/bin/nala /usr/bin/apt
+    if [ $? -eq 0 ]; then
+        echo -e "${boldGreen}Link simbólico de apt para nala criado com sucesso.${reset}"
+    else
+        echo -e "${boldRed}Falha ao criar link simbólico.${reset}"
+        exit 1
+    fi
 else
-    echo -e "${boldRed}Falha ao criar link simbólico.${reset}"
+    echo -e "${boldRed}Nala não está instalado.${reset}"
     exit 1
 fi
 
